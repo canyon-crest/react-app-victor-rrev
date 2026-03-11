d8a3e23d8b34a92294d022a2118a815f58925758
victor.rrev <victor.reverdatto@gmail.com>
2026-03-10 23:50:30 -0700

    Final React App
    this is it

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
             count is {count}
           </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          click me
         </p>
       </div>
       <p className="read-the-docs">
--- src/Nav.jsx
+++ src/Nav.jsx
@@ -6,8 +6,9 @@ function Nav() {
       <h1 className="logo">MySite</h1>
 
       <div className="nav-links">
-        <a href="/">Home</a>
+        <a href="https://www.freepik.com/free-photos-vectors/small-house">Home</a>
         <a href="https://classroom.google.com/c/ODM4MTQ1MTM2OTU1">Classroom</a>
+        <a href="https://youtu.be/IjcUiHIFEOQ?si=MZnqGakaAvEOFU31">YouTube</a>
       </div>
     </nav>
   );
