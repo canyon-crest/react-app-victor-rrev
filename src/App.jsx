bc251a41020ec73599df5ac6cf4bd7a5714929eb
victor.rrev <victor.reverdatto@gmail.com>
2026-03-10 22:01:39 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Nav from './Nav';
+import Card from './Card';
 
 import React from "react";
 import Message from "./message";
