cb90754a519611534f67d02bb4c4bb8ef2fb607c
victor.rrev <victor.reverdatto@gmail.com>
2026-03-10 21:47:15 -0700

    initial commit

---
+++ src/Nav.jsx
@@ -0,0 +1,17 @@
+import './Nav.css';
+
+function Nav() {
+  return (
+    <nav className="nav">
+      <h1 className="logo">MySite</h1>
+
+      <div className="nav-links">
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,10 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Nav />
+    <Card name ="Product 1" description="This is the first product." />
+    <Card name ="Product 2" description="This is the second product." />
+    <Card name ="Product 3" description="This is the third product." />
     <App />
   </StrictMode>,
 )
