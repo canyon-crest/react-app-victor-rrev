7f258bde4201ec266813e728321d17ee08253846
victor.rrev <victor.reverdatto@gmail.com>
2026-03-11 12:50:49 -0700

    importing nav

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Card from './card.jsx'
+import Nav from './nav.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
