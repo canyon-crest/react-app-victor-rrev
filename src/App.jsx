f634a90dce5f79eb83490b423bc10563a4b8dc04
victor.rrev <victor.reverdatto@gmail.com>
2026-03-10 22:42:22 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
-import Nav from './nav'
-import Card from './card'
+import Nav from './Nav'
+import Card from './Card'
 
 import React from "react";
 import Message from "./message";
