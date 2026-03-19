fdc1af8710a62613c0dd43cc4dfeeff2a90be949
victor.rrev <victor.reverdatto@gmail.com>
2026-03-18 23:19:09 -0700

    api

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,27 @@
+import { useState } from 'react';
+
 function Home() {
+  const [advice, setAdvice] = useState("");
+
+  const fetchAdvice = async () => {
+    try {
+      const response = await fetch('https://api.adviceslip.com/advice');
+      const data = await response.json();
+      setAdvice(data.slip.advice);
+    } catch (error) {
+      console.error('Error fetching advice:', error);
+    }
+  };
+
+
   return (
     <div>
       <h2>Home Page</h2>
       
       <p>this is the home, just as Ace is the place with the helpful hardware store</p>
+
+      <button onClick={fetchAdvice}>Get Advice</button>
+      {advice && <p>Advice: {advice}</p>}
     </div>
   )
 }
