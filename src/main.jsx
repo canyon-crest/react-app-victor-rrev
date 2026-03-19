769fd7ad25e2d5fb081a9770040300e97a1e6ad2
victor.rrev <victor.reverdatto@gmail.com>
2026-03-18 22:48:14 -0700

    import footer

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import Footer from './Footer.jsx'
 import App from './App.jsx'
 import Card from './card.jsx'
 import Nav from './nav.jsx'
